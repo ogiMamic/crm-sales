4bbb318de23fbf2114c94f7ee227674512e8e7fe
da-ni-e-l <116642398+DanielMajstorovic@users.noreply.github.com>
2024-11-24 12:54:32 +0100

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -25,7 +25,7 @@ export default function Header() {
 
   return (
     <header className="bg-white shadow-md">
-      <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
+      <div className="w-full mx-auto py-6 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
         <h1 className="text-3xl font-bold text-gray-900">{getPageTitle()}</h1>
         <UserButton afterSignOutUrl="/"/>
       </div>
